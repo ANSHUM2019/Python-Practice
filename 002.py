2f9fbd87a498ac8d1695906e75564e5bf61a4918
ANSHUM2019 <49760313+ANSHUM2019@users.noreply.github.com>
2019-04-22 09:45:58 +0530

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,10 @@
+course='I am a honest person'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('h'))
+print(course.find('a'))
+print(course.find('honest'))
+print(course.replace('honest',"honest and good"))
+print(course.replace('h','j'))
+print('person' in course)
