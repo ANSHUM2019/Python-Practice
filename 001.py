71388761ec3a0620da55fb681c27ebbba1c8ca78
ANSHUM2019 <49760313+ANSHUM2019@users.noreply.github.com>
2019-04-22 08:27:29 +0530

    Create 001.py

---
+++ 001.py
@@ -0,0 +1,9 @@
+course='I am a hardworking person'
+print(course[0])
+print(course[-1])
+print(course[-7])
+print(course[0:5])
+print(course[1:])
+print(course[:9])
+print(course[:])
+print(course[1:-1])
