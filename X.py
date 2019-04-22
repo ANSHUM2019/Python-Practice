8db39c07f49a8b6a18ce0a74a972edb8db7f483e
ANSHUM2019 <49760313+ANSHUM2019@users.noreply.github.com>
2019-04-22 07:31:51 +0530

    Create X.py

---
+++ X.py
@@ -0,0 +1,13 @@
+course='I am a good "student"'
+course_1="I am a good 'STUDENT'"
+print(course)
+print(course_1)
+course_2='''
+Hi Sunil,
+
+It's great that you helped me to do this.
+
+Thank you 
+
+'''
+print(course_2)
