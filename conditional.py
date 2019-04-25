38e22a7091ddeab7affd65c0e76c92cf6f7ffa4a
ANSHUM2019 <49760313+ANSHUM2019@users.noreply.github.com>
2019-04-25 17:38:22 +0530

    Update conditional.py

--- conditional.py
+++ conditional.py
@@ -23,3 +23,17 @@ elif is_cold:
 else :
     print("It's a lovely day")
     print("Enjoy your day")
+
+    ((ANOTHER PROGRAM))
+    
+is_hot=False
+is_cold=False
+if is_hot:
+    print("It's  a hot day")
+    print('drink alot of water')
+elif is_cold:
+    print("It's a cold day")
+    print("wear warm clothes")
+else :
+    print("It's a lovely day")
+    print("Enjoy your day")
