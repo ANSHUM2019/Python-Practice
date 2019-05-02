a1190c1bbf29b757973aba26fe7328ac5e0ab808
ANSHUM2019 <49760313+ANSHUM2019@users.noreply.github.com>
2019-05-03 00:15:31 +0530

    Create comparision.py

---
+++ comparision.py
@@ -0,0 +1,27 @@
+has_good_credit=True
+has_high_income=True
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+if has_high_income or has_good_credit:
+    print("eligible for loan")
+    
+    ((Another program))
+    
+has_good_credit=True
+has_high_income=False
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+if has_high_income or has_good_credit:
+    print("eligible for loan")
+    
+    ((Another program))
+    
+has_good_credit=False
+has_high_income=False
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+if has_high_income or has_good_credit:
+    print("eligible for loan")    
