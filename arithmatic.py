1d30d9a86a0bebec9539180530db4ba9c924bdc0
Sunil Adhya <suniladhya@live.com>
2019-05-03 17:46:40 +0530

    Update and rename arithmatic .py to arithmatic.py

--- arithmatic.py
+++ arithmatic.py
@@ -1,9 +1,9 @@
 print(10+3)
 print(10-7)
 print(11/3)
-print(11//3)
+print(11//3) # Floor Division
 print(20%6)
-print(20**2)
+print(20**2) # Exponent 9.0//2.0 = 4.0, -11//3 = -4
 x=100
 x=x+12
 print(x)
