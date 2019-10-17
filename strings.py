9192c345802a4c1e008e89de54c2784ecc8cfa21
Sunil Adhya <suniladhya@live.com>
2019-10-18 01:51:12 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -13,3 +13,19 @@ Escape Characters
 
 superpowers = ['flight', 'Cool Cape', '20/20 Vision']
 print(superpowers)
+print(*superpowers) # Concatenates the List content
+print(superpowers[0])
+superpowers.append()
+superpowers.pop([index]) #pop out the value and Deletes it form the source
+superpowers.insert(1, 'Taco Meat')
+del superpowers[1]
+superpowers.remove('flight')
+superpowers.reverse()
+superpowers.copy() # Returns all the element of the List
+superpowers.index('flight') # Returns the index of the element
+superpowers.clear()
+superpowers.count(<Value>)
+superpowers[1].count('o')
+superpowers[1].count('o',1,2)# Sustring, start, Last index
+
+
