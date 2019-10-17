d453e9d03d19698219a8aa0c7bb89b48400e5546
Sunil Adhya <suniladhya@live.com>
2019-10-17 23:56:06 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,15 @@
+print("""
+This
+is 
+multiline""")
+
+Escape Characters
+\  :Allows to creare a new line in multiline string
+\\ :Inclusion of back slash in a string
+\n :Creating a line break
+\t :Creates a tab or an indendation
+\' :single quotes
+\" :Double quotes
+
+superpowers = ['flight', 'Cool Cape', '20/20 Vision']
+print(superpowers)
